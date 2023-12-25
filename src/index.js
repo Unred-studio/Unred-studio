47060644d73937c8244279a21372739cbc62435e
Aaditya Karamchandani <unredaaditya36@gmail.com>
2023-12-25 17:26:12 -0500

    Project complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactMarkdown from 'react-markdown';
+
+
+function Show() {
+
+  const defMarkdownText = `
+  # Welcome to my React Markdown Previewer!
+  
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+  
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\\\`\`\`' && lastLine == '\\\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+  
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+  
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+  `;
+
+
+
+
+  const [markdownText, setMarkdownText] = useState(defMarkdownText);
+
+  const handleTextChange = (e) => {
+    setMarkdownText(e.target.value);
+  }
+
+  
+
+
+  return (
+    <>
+    <div id="app-container">
+    <textarea onChange={handleTextChange} id="editor" value={markdownText}></textarea>
+    <div id="preview">
+      <ReactMarkdown>{markdownText}</ReactMarkdown>
+    </div>
+    </div>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+    <Show />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
